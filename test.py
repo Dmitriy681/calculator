da41494dfdfca8dc17d33bb8f0dacf86d3b595a9
thesky68 <thesky68@inbox.ru>
2024-11-13 21:52:52 +0300

    Test commit))

--- test.py
+++ test.py
@@ -1,24 +1,37 @@
-print("Калькулятор 3000 готов!")
-print("=======================")
-print("Калькулятор 3000 готов!")
-print("-----------------------")
-print("Калькулятор 3000 готов!")
-while True:
-    print("Введите первую цифру:")
-    a = int(input())
-    print("Введите вторую цифру:")
-    b = int(input())
-    print("Что нужно сделать:")
-    c = input()
-    if c == '+':
-        x = a + b
-        print("Итог = ",x)
-    elif c == "-":
-        x = a - b
-        print("Итог = ", x)
-    elif c == '*':
-        x = a * b
-        print("Итог = ", x)
-    elif c == "/":
-        x = a / b
-        print("Итог = ", x)
+
+n = int(input())
+l = []
+
+srav = -1000
+
+for i in range(n):
+    x = input().split()
+    l.append(x)
+
+#левая часть
+print('левая часть')
+for i in range(n):
+    for j in range(n):
+        if int(i >= j) and int(i <= n - 1 -j):
+            print(l[i][j])
+
+print('правая часть')
+#правая часть
+for i in range(n):
+    for j in range(n):
+        if int(i <= j) and int(i >= n - 1 -j):
+            print(l[i][j])
+
+print('верхняя часть')
+#верхняя часть
+for i in range(n):
+    for j in range(n):
+        if int(i <= j) and int(i <= n - 1 -j):
+            print(l[i][j])
+
+print('нижняя часть')
+#нижняя часть
+for i in range(n):
+    for j in range(n):
+        if int(i >= j) and int(i >= n - 1 -j):
+            print(l[i][j])
